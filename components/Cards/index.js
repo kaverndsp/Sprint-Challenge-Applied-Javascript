38ef659402bb5125028b9fcf527a2872061d0e93
kaverndsp <kaverndsp@users.noreply.github.com>
2020-01-17 11:55:37 -0600

    adding questions

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,61 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const grab = document.querySelector('.cards-container');
+
+
+axios
+.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    const data = response.data.articles;
+    data.javascript.forEach(item =>{
+        grab.append(Card(item));
+    })
+
+    data.bootstrap.forEach(item => {
+        grab.append(Card(item));
+    })
+    
+    data.technology.forEach(item => {
+        grab.append(Card(item));
+    })
+
+    data.jquery.forEach(item => {
+        grab.append(Card(item));
+    })
+    
+    data.node.forEach(item => {
+        grab.append(Card(item));
+    })
+console.log(response.data.articles);
+})
+
+function Card(data){
+    const newCard = document.createElement('div');
+    const newHeadline = document.createElement('div');
+    const newAuthor = document.createElement('div');
+    const newImgCont = document.createElement('div');
+    const newImg = document.createElement('img');
+    const newName = document.createElement('span');
+
+    newCard.append(newHeadline);
+    newCard.append(newAuthor);
+    newAuthor.append(newImgCont);
+    newImgCont.append(newImg);
+    newAuthor.append(newName);
+
+    newCard.classList.add('card');
+    newHeadline.classList.add('headline');
+    newAuthor.classList.add('author');
+    newImgCont.classList.add('img-container');
+
+    newHeadline.textContent = data.headline;
+    newImg.src = data.authorPhoto;
+    newName.textContent = data.authorName;
+
+    return newCard;
+
+    
+
+}
